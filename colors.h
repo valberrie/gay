98ee2656ab3999551a34f388cdac2745a9d4535f
valberrie <valberrie@proton.me>
2024-02-07 13:43:12 -0800

    init

---
+++ colors.h
@@ -0,0 +1,17 @@
+// define 8-bit colors and classic ansi escape colors
+
+#define RED     "\033[48;5;196m"
+#define ORANGE  "\033[48;5;202m"
+#define YELLOW  "\033[48;5;226m"
+#define GREEN   "\033[48;5;34m"
+#define BLUE    "\033[48;5;21m"
+#define PURPLE  "\033[48;5;93m"
+#define WHITE   "\033[48;5;231m"
+#define BLACK   "\033[48;5;16m"
+
+#define FUCHSIA "\033[48;5;197m"
+#define PINK    "\033[48;5;201m"
+
+#define LBLUE   "\033[48;5;39m"
+#define HPINK   "\033[48;5;218m"
+
---
+++ gay.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include "colors.h"
+
+struct winsize sz;
+
+void draw_line(char *cc) {
+  int s = 0;
+  printf("\n");
+  while(s <= (sz.ws_col - 1)) {
+    printf(cc);
+    printf(" ");
+    s++;
+  }
+}
+
+void draw_flag(int f) {
+  switch(f) {
+    case 1:
+      draw_line(FUCHSIA);
+      draw_line(FUCHSIA);
+      draw_line(PURPLE);
+      draw_line(BLUE);
+      draw_line(BLUE);
+    break;
+
+    case 2:
+      draw_line(LBLUE);
+      draw_line(HPINK);
+      draw_line(WHITE);
+      draw_line(HPINK);
+      draw_line(LBLUE);
+    break;
+
+    default:
+      draw_line(RED);
+      draw_line(ORANGE);
+      draw_line(YELLOW);
+      draw_line(GREEN);
+      draw_line(BLUE);
+      draw_line(PURPLE);
+  }
+  printf("\n\n");
+}
+
+int main(int argc, char *argv[]) {
+  const char *flags[3] = {"gay", "bi", "trans"};
+
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &sz); 
+  
+  int a;
+  if (argv[1] == NULL) {
+    a = 0;
+  } else {
+    for(int i = 0; i < sizeof(flags) / sizeof(flags[0]); i++) {
+      if (strcmp(argv[1], flags[i]) == 0) {
+        a = i;
+      }
+    }
+  }
+  draw_flag(a);
+  return 0;
+}
